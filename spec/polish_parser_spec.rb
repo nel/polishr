2a595ed2981d12b0f638f5a3b9214a48ace71197
Renaud (Nel) Morvan <nel@w3fu.com>
2011-01-06 04:40:09 +0100

    Add parenthesis support and more forgiving syntax

--- spec/polish_parser_spec.rb
+++ spec/polish_parser_spec.rb
@@ -1,16 +1,34 @@
 require File.dirname(__FILE__) + '/../polish_parser'
 
+def calculate(string)
+  PolishParser.new.calculate(string)
+end
+
 describe Polish do
   it "Should support addition" do
-    PolishParser.new.calculate("+ 1 2").should == 3
+    calculate("+ 1 2").should == 3
   end
   
   it "Should support recusive addition" do
-    PolishParser.new.calculate("+ + 1 2 3").should == 6
-    PolishParser.new.calculate("+ 1 + 2 3").should == 6
+    calculate("+ + 1 2 3").should == 6
+    calculate("+ 1 + 2 3").should == 6
   end
 
   it "Should support multiple operator" do
-    PolishParser.new.calculate("+ 1 * 4 5").should == 21
+    calculate("+ 1 * 4 5").should == 21
   end
+
+  it "Should be permissive in syntax" do
+    calculate(" +1*3 4 ").should == 13
+  end
+
+  it "Should support operation priority" do
+    calculate("* - 2 1 1").should == 1
+    calculate("+ 1 * 1 2").should == 3
+  end
+
+  it "Should support parenthesis" do
+    calculate("* (+ 1 3) 2").should == 8
+  end
+
 end
